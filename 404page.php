e83aacd398054249619c738ff58daead817a1afa
DG1243 <DGLADMA1@stu.chi.ac.uk>
2023-03-20 18:05:05 +0000

    404 Page

---
+++ 404page.php
@@ -0,0 +1,13 @@
+<?php require_once("connection.php");
+require("commonHead.php") ?>
+
+<title>Testmark: Temperature Sensors and Instruments</title>
+</head>
+
+<?php require("commonNav.php") ?>
+
+<h1>404</h1>
+
+</body>
+
+</html>
