ea2037b78bc894738e33397e411411f9462f2985
DG1243 <DGLADMA1@stu.chi.ac.uk>
2023-03-20 10:58:32 +0000

    Navbar

--- commonNav.php
+++ commonNav.php
@@ -6,8 +6,19 @@ echo '<body>
     <a href="index.php" class="navbar-brand" id="Brand">Testmark</a>
     <ul class="navbar-nav">';
 
-$querry = "SELECT COUNT(id) AS categoryCount FROM category";
-$statement = $db->query($querry);
-$categoryCount = $statement->fetchColumn();
-echo $categoryCount;
-    ?>
+$querry = "SELECT * from category";
+$result = $db->query($querry);
+foreach ($result as $row) {
+    echo '<li class="nav-item">
+    <a href="testmark-' . $row['name'] . '.php' . '" class="nav-link" id="NavL">' . $row['name'] . '</a>
+    </li>';
+}
+
+echo '<li class="nav-item">
+<a href="Testmark-about.html" class="nav-link" id="NavL5">About Us</a>
+</li>
+<li class="nav-item">
+<a href="Testmark-technical.html" class="nav-link" id="NavL6">Technical Information</a>
+</li>
+</ul>
+</nav>';
